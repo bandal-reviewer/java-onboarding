ae47958c3a4773544f740ebeb61d2f8df6c4c18e
nakangmin <92040803+nakangmin@users.noreply.github.com>
2023-07-02 22:54:50 +0900

    나강민 [Java-onboarding] 제출합니다.
    5, 6번(틀린답)을 포함하고 기간맞춰 제출합니다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,33 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder sb = new StringBuilder(cryptogram);
+
+        removeDuplicates(sb);
+
+        return sb.toString();
+
     }
+    private static void removeDuplicates(StringBuilder sb) {
+        boolean removed = true;
+
+        while (removed) {
+            removed = false;
+            int length = sb.length();
+            int i = 0;
+
+            while (i < length - 1) {
+                if (sb.charAt(i) == sb.charAt(i + 1)) {
+                    sb.delete(i, i + 2);
+                    length -= 2;
+                    removed = true;
+                } else {
+                    i++;
+                }
+            }
+        }
+    }
+
+
+
 }
--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,48 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Set<String> invalidEmails = new HashSet<>(); // 중복된 이메일을 저장할 Set
+
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+
+            if (hasDuplicateCharacters(nickname)) {
+                invalidEmails.add(email); // 중복된 글자가 있을 경우 이메일을 추가
+            }
+        }
+
+        return sortAndRemoveDuplicates(new ArrayList<>(invalidEmails));
     }
+    private static boolean hasDuplicateCharacters(String nickname) {
+        // 같은 글자가 연속적으로 포함되어 있는지 확인
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            if (nickname.charAt(i) == nickname.charAt(i + 1)) {
+                return true; // 중복된 글자가 있으면 true 반환
+            }
+        }
+        return false; // 중복된 글자가 없으면 false 반환
+    }
+
+    private static List<String> sortAndRemoveDuplicates(List<String> emails) {
+        emails.sort(null); // 이메일 목록을 오름차순으로 정렬
+        List<String> result = new ArrayList<>();
+        String prevEmail = null;
+
+        for (String email : emails) {
+            if (!email.equals(prevEmail)) {
+                result.add(email); // 중복된 이메일 제거하고 결과 리스트에 추가
+                prevEmail = email;
+            }
+        }
+
+        return result;
+    }
+
 }
